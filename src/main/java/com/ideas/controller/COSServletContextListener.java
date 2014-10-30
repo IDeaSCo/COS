f66a5dce04522c4016b981c8e2940dd9d699bc32
singh-abhishek <singhkmabhishek@gmail.com>
2014-10-30 19:56:02 +0530

    DBCP Implemented.

--- src/main/java/com/ideas/controller/COSServletContextListener.java
+++ src/main/java/com/ideas/controller/COSServletContextListener.java
@@ -6,28 +6,62 @@ import java.sql.SQLException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
 import com.ideas.domain.Repository;
 
 public class COSServletContextListener implements ServletContextListener {
-    private Repository repository;
+	private Repository repository;
+	private DataSource dataSource;
 
-	public void contextInitialized(ServletContextEvent sce)  {
-    	ServletContext servletContext = sce.getServletContext();
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext servletContext = sce.getServletContext();
+		String username = servletContext.getInitParameter("username");
+		String password = servletContext.getInitParameter("password");
+		String driverClassName = servletContext.getInitParameter("driver");
+		String url = servletContext.getInitParameter("url");
+		dataSource = setupDataSource(username, password, driverClassName, url);
 		try {
-			Class.forName(servletContext.getInitParameter("driver"));
-			Connection connection = DriverManager.getConnection(servletContext.getInitParameter("url"),
-																servletContext.getInitParameter("username"),
-																servletContext.getInitParameter("password"));
-			repository = new Repository(connection);
-			servletContext.setAttribute("repository", repository);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			repository = new Repository(dataSource.getConnection());
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		servletContext.setAttribute("repository", repository);
+
+//		try {
+//			Class.forName(servletContext.getInitParameter("driver"));
+//			Connection connection = DriverManager.getConnection(
+//					servletContext.getInitParameter("url"),
+//					servletContext.getInitParameter("username"),
+//					servletContext.getInitParameter("password"));
+//			repository = new Repository(connection);
+//			servletContext.setAttribute("repository", repository);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+	}
+
+	private static DataSource setupDataSource(String username, String password,
+			String driverClassName, String url) {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName(driverClassName);
+		ds.setUsername(username);
+		ds.setPassword(password);
+		ds.setUrl(url);
+		return ds;
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		BasicDataSource bds = (BasicDataSource) dataSource;
+		try {
+			bds.close();
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-    public void contextDestroyed(ServletContextEvent sce)  { 
-    }
-	
 }
