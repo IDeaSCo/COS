c1a126e85a8fe59cd982117e42a482f3e23efa35
Sonam <sonam.rasal@ideas.com>
2014-11-07 15:33:37 +0530

    Refactoring

--- src/main/java/com/ideas/controller/RouteOptimizationController.java
+++ src/main/java/com/ideas/controller/RouteOptimizationController.java
@@ -20,26 +20,27 @@ import com.ideas.routeOptimization.RouteOptimizer;
 public class RouteOptimizationController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private RouteOptimizer routeOptimizer;
-
+	private ControllerHelper helper;
+	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		routeOptimizer = (RouteOptimizer) config.getServletContext().getAttribute("routeOptimizer");
+		helper = (ControllerHelper) config.getServletContext().getAttribute("helper");
 	}
 
 	public RouteOptimizationController() {
 		super();
 	}
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ArrayList<TreeMap<Time, ArrayList<ArrayList<DataPoint>>>> timeMapList = null;
 		try {
 			timeMapList = routeOptimizer.optimizeRoute();
 			request.setAttribute("timeMapList", timeMapList);
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/vendorMapSolutions.jsp");
+			helper.sendRequest(request, response, "vendorMapSolutions.jsp");
+/*			RequestDispatcher dispatcher = request.getRequestDispatcher("/vendorMapSolutions.jsp");
 			dispatcher.forward(request, response);
-		} catch (ClassNotFoundException e) {}
+*/		} catch (ClassNotFoundException e) {}
 		  catch (SQLException e) {}
 	}
 
